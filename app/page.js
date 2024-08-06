03bb2b030af230a1eb5db8058a2a2cc9987be87f
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-06 12:11:51 -0400

    set up structure for chat page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,81 @@
+"use client";
+
+import { Box, Button, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+
 export default function Home() {
-  return;
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content:
+        "Hello, I am Baymax, your personal AI companion. How can I help you today?",
+    },
+  ]);
+  const [message, setMessage] = useState("");
+
+  const sendMessage = async () => {
+    // We'll implement this function in the next section
+  };
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        direction={"column"}
+        width="500px"
+        height="700px"
+        border="1px solid black"
+        p={2}
+        spacing={3}
+      >
+        <Stack
+          direction={"column"}
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          maxHeight="100%"
+        >
+          {messages.map((message, index) => (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent={
+                message.role === "assistant" ? "flex-start" : "flex-end"
+              }
+            >
+              <Box
+                bgcolor={
+                  message.role === "assistant"
+                    ? "primary.main"
+                    : "secondary.main"
+                }
+                color="white"
+                borderRadius={16}
+                p={3}
+              >
+                {message.content}
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+        <Stack direction={"row"} spacing={2}>
+          <TextField
+            label="Message"
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained" onClick={sendMessage}>
+            Send
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 }
